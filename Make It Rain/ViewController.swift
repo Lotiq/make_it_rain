2d4ad0e50ef4faa8207e865cecc38d8197e1bd1c
Timothy L <tsimafei.lobiak@gmail.com>
2019-04-13 12:46:24 -0400

    first commit

---
+++ Make It Rain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Make It Rain
+//
+//  Created by Timothy on 4/13/19.
+//  Copyright © 2019 Timothy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
