8b5074edddd70a43bf3cc8764c3ef6d7a60f8251
Timothy L <tsimafei.lobiak@gmail.com>
2019-04-14 13:42:31 -0400

    First View Portrait Design

---
+++ Make It Rain/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  Make It Rain
+//
+//  Created by Timothy on 4/13/19.
+//  Copyright © 2019 Timothy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, a: Float = 1) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(a))
+    }
+    
+    convenience init(hex:Int) {
+        self.init(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff)
+    }
+    
+    struct themeColor{
+        static let main = #colorLiteral(red: 0.1631833613, green: 0.3730655909, blue: 0.2650748491, alpha: 1)
+        static let secondary = #colorLiteral(red: 0.5862457752, green: 0.7702537179, blue: 0.4708444476, alpha: 1)
+        static let gray = #colorLiteral(red: 0.8014289141, green: 0.7830495238, blue: 0.7300147414, alpha: 1)
+        static let extra = #colorLiteral(red: 0.9134007692, green: 0.8268355727, blue: 0.4537941813, alpha: 1)
+        static let gold = #colorLiteral(red: 0.8106517196, green: 0.6584199667, blue: 0.1929169595, alpha: 1)
+    }
+}
--- Make It Rain/ViewController.swift
+++ Make It Rain/ViewController.swift
@@ -7,14 +7,94 @@
 //
 
 import UIKit
+import HGCircularSlider
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var slider: CircularSlider!
+    @IBOutlet weak var playBarButton: UIBarButtonItem!
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var cashTextField: UITextField!
+    
+    var currencies: [String:String] = ["dollar":"$","peso":"$"]
+    var selectedCurrency: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSlider(slider: slider)
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "money.jpg")!)
+        //navigationController?.navigationBar.backgroundColor = UIColor.themeColor.main
+        navigationController?.navigationBar.barTintColor = UIColor.themeColor.main
         // Do any additional setup after loading the view, typically from a nib.
+        playBarButton.tintColor = UIColor.themeColor.extra
+        menuBarButton.tintColor = UIColor.themeColor.extra
+        //menuBarButton.image
+        cashTextField.delegate = self
+        slider.addTarget(self, action: #selector(updateCashValue), for: .valueChanged)
+        selectedCurrency = currencies["dollar"]!
+        
     }
-
-
+    
+    
+    func setupSlider(slider: CircularSlider){
+        slider.minimumValue = 0
+        slider.maximumValue = 10000
+        slider.diskColor = .clear
+        slider.trackFillColor = UIColor.themeColor.main
+        slider.thumbRadius = 16
+        slider.trackColor = UIColor.themeColor.gray
+        slider.backtrackLineWidth = 6
+        slider.lineWidth = 14
+        slider.endThumbStrokeColor = UIColor.themeColor.main
+        slider.endThumbTintColor = UIColor.themeColor.extra
+        slider.thumbLineWidth = 8
+        slider.endPointValue = 300
+        slider.endThumbStrokeHighlightedColor = UIColor.themeColor.main
+        slider.backgroundColor = .clear
+        //slider.trackShadowColor = .red
+        
+        
+    }
+    
+    @objc func updateCashValue(){
+        let newValue = Int(slider.endPointValue)
+        cashTextField.text = selectedCurrency + "\(newValue)"
+        
+    }
+    
+    
+    
+    func updateSlider(num: Int){
+        slider.endPointValue = num < Int(slider.maximumValue) ? CGFloat(num) : slider.maximumValue - 1
+    }
+    
+    // MARK: TextField Delegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string)
+        let modifiedText = newText.replacingOccurrences(of: selectedCurrency, with: "")
+        
+        guard let num = Int(modifiedText) else {
+            if (modifiedText == ""){
+                textField.text = selectedCurrency
+                updateSlider(num: 0)
+            }
+            return false
+        }
+        
+        textField.text = selectedCurrency + "\(num)"
+        updateSlider(num: num)
+        
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField.text == selectedCurrency){
+            textField.text = selectedCurrency + "0"
+        }
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
